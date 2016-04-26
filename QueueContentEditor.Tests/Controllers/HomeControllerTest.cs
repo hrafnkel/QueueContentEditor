31ff6fe7333df7654f9fe0bf4a8a49f1f644c195
Crowther, Tim <tim.crowther@burrows.yr.com>
2016-04-26 10:48:01 +0100

    Removed unused "using directives"

--- QueueContentEditor.Tests/Controllers/HomeControllerTest.cs
+++ QueueContentEditor.Tests/Controllers/HomeControllerTest.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Web.Mvc;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using QueueContentEditor;
-using QueueContentEditor.Controllers;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace QueueContentEditor.Tests.Controllers
 {
--- QueueContentEditor.Tests/QueueHelperTests.cs
+++ QueueContentEditor.Tests/QueueHelperTests.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Messaging;
-using System.Text;
-using System.Threading.Tasks;
-using Moq;
 using NUnit.Framework;
 using Repository.Queue;
 
